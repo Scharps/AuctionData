3facc736f631f0dccb5c26df2989a46c9ce73052
Samuel James <507.s94j@gmail.com>
2023-10-19 13:30:10 -0300

    Restructure db schema

--- AuctionData.ApplicationCore/Entities/Auction/Auction.cs
+++ AuctionData.ApplicationCore/Entities/Auction/Auction.cs
@@ -1,10 +1,42 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace AuctionData.Application.Entities.Auction;
 
 public sealed class Auction : Entity
 {
-    public ItemListing ItemListing { get; set; } = null!;
+    public ConnectedRealm ConnectedRealm { get; set; } = null!;
+    public DateTime FirstSeen { get; set; }
+    public DateTime? LastSeen { get; set; }
+    public Item.Item Item { get; set; } = null!;
+    public string InternalBonuses { get; private set; } = string.Empty;
+    [NotMapped]
+    public long[] Bonuses
+    {
+        get
+        {
+            return Array.ConvertAll(InternalBonuses.Split(';'), long.Parse);
+        }
+        set
+        {
+            InternalBonuses = string.Join(';', value ?? Array.Empty<long>());
+        }
+    }
+    public ICollection<Modifier> Modifiers { get; set; } = null!;
+    public long? Bid { get; set; }
     public long Buyout { get; set; }
     public long Quantity { get; set; }
-    public TimeLeft TimeLeft { get; set; }
-    public long? Bid { get; set; }
+    public DateTime ExpectedExpiry { get; set; }
+}
+
+public sealed class ConnectedRealm : Entity
+{
+    public int ConnectedRealmId { get; set; }
+    public Region Region { get; set; }
+}
+
+public enum Region
+{
+    EU,
+    US,
+    TW,
 }
