443ff2323c702e4d7f21d98d582fdc35c8d4f250
Samuel James <507.s94j@gmail.com>
2023-10-20 23:56:10 +0100

    Add file logging

--- AuctionData.ApplicationCore/Program.cs
+++ AuctionData.ApplicationCore/Program.cs
@@ -4,9 +4,15 @@ using AuctionData.Application.Features;
 using AuctionData.Application.Services.BlizzardApi;
 using AuctionData.Application.Services.BlizzardApi.Extensions;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.File("log.txt", Serilog.Events.LogEventLevel.Information)
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<BlizzardAppOptions>(builder.Configuration.GetRequiredSection("BlizzardAppOptions"));
+builder.Services.AddSerilog();
 builder.Services.AddOAuthTokenManager(builder.Configuration);
 builder.Services.AddBlizzardClient();
 builder.Services.AddDbContext<AuctionDbContext>(opt => opt.UseSqlite(builder.Configuration.GetConnectionString("Default"), optionsBuilder => optionsBuilder.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName)));
